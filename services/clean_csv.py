87386374292425a9db10913ae1dbd19dedb6175a
jorferg <jorfer1945@ciencias.unam.mx>
2025-08-06 22:50:48 -0600

    Added functionality to init_db for creating databases

--- services/clean_csv.py
+++ services/clean_csv.py
@@ -32,13 +32,21 @@ def clean_csv_in_chunks(input_path, output_path, chunk_size=100000):
         chunk_no += 1
 
 def create_csv_from_cleaned(first_chunk, input_path, output_path, chunk_size=100000):
-    chunks = pd.read_csv(input_path, chunksize=chunk_size, encoding='utf-8', encoding_errors='replace',
-                        on_bad_lines='warn', dtype=str, engine='python', low_memory=True)
-    for i, chunk in enumerate(chunks):
-        # Write to output file
+    chunk_no = 0    
+    for chunk in pd.read_csv(input_path, chunksize=chunk_size, encoding="utf-8", encoding_errors='replace',
+                        on_bad_lines='warn', dtype=str, engine='python', low_memory=True):
         mode = 'w' if first_chunk else 'a'
         chunk.to_csv(output_path, mode=mode, index=False, header=first_chunk, encoding='utf-8')
         first_chunk = False
-        
-        # Status update
-        print(f"Processed chunk {i+1} ({chunk_size * (i+1)} rows)")
+        print(f"Processed chunk {chunk_no+1} ({chunk_size * (chunk_no+1)} rows)")
+        chunk_no += 1
+
+def db_columns_to_lowercase(table_name, con):
+    columns_to_lower = con.sql(f"SELECT * FROM {table_name}").columns
+    for column in columns_to_lower:
+        if isinstance(column, str):
+            temp_column_name = column
+            column_name_lower = temp_column_name.lower()
+            if (temp_column_name != column_name_lower):
+                con.sql(f"""ALTER TABLE {table_name} RENAME COLUMN {temp_column_name} TO 
+                            {column_name_lower}""")
