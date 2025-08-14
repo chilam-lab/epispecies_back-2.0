469344639845f2ae2f53e38cf6e5e343173de2b4
jorferg <jorfer1945@ciencias.unam.mx>
2025-08-14 02:09:37 -0600

    Optimized init_db so it doesnt iterate csv files two times.

--- services/file_helper_functions.py
+++ services/file_helper_functions.py
@@ -1,5 +1,5 @@
 from services.clean_csv import clean_csv_in_chunks, create_csv_from_cleaned
-from os import listdir, remove
+from os import listdir, remove, rename
 from os.path import join, exists
 
 """
@@ -25,37 +25,9 @@ def get_csv_in_directory_to_clean() -> bool:
         if file.endswith(".csv"):
             csv_file_dir = join(csvs_dir, file)
             name_of_cleaned_file = join(dir_clean_csv, file)
+            name_of_cleaned_file = name_of_cleaned_file[:-4] + "_db.csv"
             if exists(name_of_cleaned_file):
                 remove(name_of_cleaned_file)
             clean_csv_in_chunks(csv_file_dir, name_of_cleaned_file)
             cleaned_a_csv = True
     return cleaned_a_csv
-
-
-def get_cleaned_csv_files() -> bool:
-    """
-    Get all csv files in the cleanCSV directory of the project, then 
-    creates a new file that contains all informations of the csvs.
-
-    Returns:
-    
-    --- True if the file was created successfully. \n
-    --- False if directory is empty, no csv files were found or file was not created.
-    """
-    file_created = False
-    if len(listdir("cleanedCSV/")) == 0:
-        return file_created
-    first_chunk = True
-    csv_file_dir = ""
-    dir_clean_csv = "cleanedCSV"
-    csv_to_table = join(dir_clean_csv, "csv_to_table_file.csv")        
-    if exists(csv_to_table):
-        remove(csv_to_table)
-    for file in listdir("cleanedCSV/"):
-        if file.endswith(".csv"):
-            csv_file_dir = join(dir_clean_csv, file)
-            create_csv_from_cleaned(first_chunk, csv_file_dir, csv_to_table)
-            first_chunk = False
-    if exists(csv_to_table):
-        file_created = True
-    return file_created
