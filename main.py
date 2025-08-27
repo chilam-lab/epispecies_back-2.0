21807d5ecbbf8246c17017732c2e2484a1f0d9ae
jorferg <jorfer1945@ciencias.unam.mx>
2025-08-26 20:49:02 -0600

    Small fixes to get_variables EndPoint for JSON

--- main.py
+++ main.py
@@ -395,14 +395,16 @@ async def get_variables(con: DuckDBConn = Depends(get_db)):
     try:
         result = con.sql(f"SELECT DISTINCT CONCAT(" \
                          "'ID: ', cve_enfermedad, ', name: ', enfermedad, ', level_size: 3, filter_fields: [“anio”, “sexo”, “edad”, “muncipio”], available_grids: [18, 19]')" \
-                         ", FROM ENFERMEDADES ORDER BY cve_enfermedad;").fetchall()
+                         " FROM ENFERMEDADES ORDER BY cve_enfermedad;").fetchall()
+        
         result += con.sql(f"SELECT DISTINCT CONCAT(" \
                          "'ID: ', cve_grupo, ', name: ', grupo, ', level_size: 2, filter_fields: [“anio”, “sexo”, “edad”, “muncipio”], available_grids: [18, 19]')" \
-                         ", FROM ENFERMEDADES ORDER BY cve_grupo;").fetchall()
+                         " FROM ENFERMEDADES ORDER BY cve_grupo;").fetchall()
+        
         result += con.sql(f"SELECT DISTINCT CONCAT(" \
                          "'ID: ', cve_causa_def, ', name: ', causa_def, ', level_size: 1, filter_fields: [“anio”, “sexo”, “edad”, “muncipio”], available_grids: [18, 19]')" \
-                         ", FROM ENFERMEDADES ORDER BY cve_causa_def;").fetchall()
-        return jsonable_encoder({"Variables": result})
+                         " FROM ENFERMEDADES ORDER BY cve_causa_def;").fetchall()
+        return json.dumps(result, ensure_ascii=False, indent= 1)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Query error: {str(e)}")
 
